427897aa9883715a6baf2f13ce329a988e280587
zd3g07 <zd3g07@gmail.com>
2019-08-30 11:29:56 +0900

    titles修正

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
        <?php if (have_posts()): ?>
        <?php while(have_posts()): the_post(); ?>
          <article class="post">
-           <h2>
+           <h2>かきくけこ
              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
            </h2>
            <p class="postmeta">

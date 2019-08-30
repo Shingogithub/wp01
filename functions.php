6c4b0edb6043362eac99173abf56016fcd5583fd
zd3g07 <zd3g07@gmail.com>
2019-08-30 11:25:08 +0900

    first commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* カスタムメニューの登録
+*/
+register_sidebar(
+    array(
+        'before_widget' => '<section class="widget">',
+        'after_widget' => '</nav></section>',
+        'after_title' => '</h2><nav>'
+    )
+    );
---
+++ page.php
@@ -0,0 +1,64 @@
+<?php get_header(); ?>
+      <article class="post">
+        <h2>
+          <a href="about.html">About</a>
+        </h2>
+        <div class="entry">
+          <p><img class="size-medium alignright" src="ToBeUploaded/comet_halley.jpg" alt="" width="300" height="208"></p>
+          <p>1986年、小学生の時にハレー彗星を見て以来、すっかり宇宙のとりこになってしまった管理人が、宇宙に関する様々なことを日々書き連ねていくブログです。</p>
+          <p>宇宙に関する知識、宇宙に関するニュース等、いろいろなトピックを配信していきますので、興味のある方は是非チェックしてください。 コメント大歓迎です！</p>
+        </div>
+      </article><!-- post -->
+    </div><!-- contents -->
+    <aside>
+      <div class="widget widget_search">
+        <form method="get" id="searchform" action="">
+        <div>
+          <label class="screen-reader-text" for="s">検索:</label>
+          <input type="text" value="" name="s" id="s">
+          <input type="submit" id="searchsubmit" value="検索">
+        </div>
+        </form>
+      </div>
+      <section class="widget">
+        <h2 class="widgettitle">最近の投稿</h2>
+        <nav>
+          <ul>
+            <li><a href="#" title="宇宙の年齢">宇宙の年齢</a></li>
+            <li><a href="#" title="宇宙の大きさ">宇宙の大きさ</a></li>
+            <li><a href="post.html" title="宇宙論">宇宙論</a></li>
+          </ul>
+        </nav>
+      </section>
+      <section class="widget">
+        <h2 class="widgettitle">最近のコメント</h2>
+        <nav>
+          <ul id="recentcomments">
+            <li class="recentcomments"><a href="#">宇宙論</a> に <a href='http://www.example.com' rel='external nofollow' class='url'>山田太郎</a> より</li>
+          </ul>
+        </nav>
+      </section>
+      <section class="widget">
+        <h2 class="widgettitle">アーカイブ</h2>
+        <nav>
+          <ul>
+            <li><a href='#' title='2012年4月'>2012年4月</a></li>
+          </ul>
+        </nav>
+      </section>
+      <section class="widget">
+        <h2 class="widgettitle">カテゴリー</h2>
+        <nav>
+          <ul>
+            <li class="cat-item"><a href="#" title="未分類 に含まれる投稿をすべて表示">未分類</a></li>
+          </ul>
+        </nav>
+      </section>
+    </aside>
+  </main>
+  <footer>
+    <p>Copyright &copy; ZDRiVE, K.K. All rights reserved.</p>
+  </footer>
+</div>
+</body>
+</html>
